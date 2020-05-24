7a15c862dd01ad566b87537cf0433dc4e9285baa
Hasan Ibraheem <hasantiny@gmail.com>
2020-05-24 15:45:23 -0400

    Use clap for argument parsing and add delay, window to type command

--- oxdotool/src/main.rs
+++ oxdotool/src/main.rs
@@ -1,46 +1,73 @@
+use clap::{Arg, App, SubCommand, AppSettings, ArgMatches};
 use std::time::Duration;
+use std::str::FromStr;
+use std::num::ParseIntError;
+use std::error::Error;
 
 fn main() {
-    let args = std::env::args().collect::<Vec<String>>();
+    let matches = App::new("oxdotool")
+        .version("0.1.0")
+        .about("Emulates inputs in X")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
 
-    if args.len() < 2 {
-        print_usage(&args[0]);
-        return;
-    }
+        .subcommand(SubCommand::with_name("type")
+            .version("0.1.0")
+            .about("sends keyboard input")
+            .arg(Arg::with_name("text")
+                .required(true)
+                .help("Text to type out"))
+            .arg(Arg::with_name("window")
+                .short("w")
+                .long("window")
+                .help("X window to send input to")
+                .takes_value(true))
+            .arg(Arg::with_name("delay")
+                .short("d")
+                .long("delay")
+                .help("Delay between each keystroke in milliseconds")
+                .takes_value(true)))
 
-    match args[1].as_str() {
-        "help" => print_help(&args[0]),
+        .get_matches();
 
-        "type" => type_command(&args),
+    let (cmd, args) = matches.subcommand();
 
-        _ => {
-            println!("Command {} not found.\n", args[1]);
-            print_help(&args[0]);
-        }
+    let result = match cmd {
+        "type" => type_command(args.unwrap()),
+
+        _ => unimplemented!("What"),
+    };
+
+    if let Err(e) = result {
+        e.exit();
     }
 }
 
-fn type_command(args: &[String]) {
-    let to_type = &args[2..];
+fn clap_parse_int<I: FromStr<Err=ParseIntError>>(input: &str) -> Result<I, clap::Error> {
+    I::from_str(input).map_err(|e| {
+        clap::Error::with_description(&e.to_string(), clap::ErrorKind::ValueValidation)
+    })
+}
 
-    if to_type.is_empty() {
-        println!("Try typing something after \"type\"");
-        return;
+fn clap_parse_optional_int<I: FromStr<Err=ParseIntError>>(input: Option<&str>) -> Result<Option<I>, clap::Error> {
+    if let Some(a) = input {
+        clap_parse_int::<I>(a).map(Some)
+    } else {
+        Ok(None)
     }
+}
 
-    let full_text = to_type.join(" ");
+fn clap_parse_default_int<I: FromStr<Err=ParseIntError>>(input: Option<&str>, default: I) -> Result<I, clap::Error> {
+    clap_parse_optional_int(input).map(|o|o.unwrap_or(default))
+}
+
+fn type_command(args: &ArgMatches) -> Result<(), clap::Error> {
+    let text = args.value_of("text").unwrap();
+    let window: u32 = clap_parse_default_int(args.value_of("window"), 0)?;
+    let delay: u64 = clap_parse_default_int(args.value_of("delay"), 0)?;
 
     let ox = oxdo::OxDo::new(None);
-    ox.enter_text_window(0, &full_text, Duration::from_millis(0));
-}
+    ox.enter_text_window(window, text, Duration::from_millis(delay));
 
-fn print_usage(arg0: &str) {
-    eprintln!("Usage: {} COMMAND ARGS", arg0);
-    eprintln!("See {} help for a list of commands", arg0);
-}
-
-fn print_help(arg0: &str) {
-    println!("Usage: {} COMMAND ARGS", arg0);
-    println!("Available commands:");
-    println!(" - type <text to type>");
+    Ok(())
 }
